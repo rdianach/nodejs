c354ed2126b1a0409a79402b69dae0caf9b79293
Amardian <xdianamarx@gmail.com>
2022-09-29 16:18:46 +0700

    belajar_nodejs

---
+++ 03_package_manager/lodash.js
@@ -0,0 +1,6 @@
+import _ from "lodash"; // underscore
+
+const name = "AMARDIAN ACHMAD";
+const result = _.capitalize(name);
+
+console.info(result);
---
+++ 03_package_manager/package.js
@@ -0,0 +1,9 @@
+import {sayHello, sum} from "belajar-nodejs-npm-library-rdianach"
+import {max, min} from "belajar-nodejs-npm-library-rdianach/number"
+
+console.info(sayHello("Amardian Achmad"));
+
+console.info(sum([10,10,10,10,10]));
+
+console.info(min(10, 20));
+console.info(max(10, 20));
